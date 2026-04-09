23e71756c9dca4620a300699fb6170d31faf1581
ilNick7 <150431261+ilNick7@users.noreply.github.com>
2026-04-09 15:31:29 +0200

    Update templates.js

--- js/templates.js
+++ js/templates.js
@@ -15,20 +15,16 @@
           exercises: [
             { id: "squat", name: "Squat", setsTarget: 4, repsTarget: "5-8", rest: 150 },
             { id: "bench", name: "Panca piana", setsTarget: 4, repsTarget: "6-8", rest: 120 },
-            { id: "row", name: "Rematore", setsTarget: 3, repsTarget: "8-10", rest: 120 },
-            { id: "rdl", name: "Stacco rumeno", setsTarget: 3, repsTarget: "6-10", rest: 150 },
-            { id: "curl", name: "Curl", setsTarget: 2, repsTarget: "10-12", rest: 75 }
+            { id: "row", name: "Rematore", setsTarget: 3, repsTarget: "8-10", rest: 120 }
           ]
         },
         {
           id: "fb-b",
           name: "Full Body B",
           exercises: [
-            { id: "dead", name: "Stacco (tecnica/volume)", setsTarget: 3, repsTarget: "3-5", rest: 180 },
+            { id: "dead", name: "Stacco", setsTarget: 3, repsTarget: "3-5", rest: 180 },
             { id: "ohp", name: "Military press", setsTarget: 4, repsTarget: "6-10", rest: 120 },
-            { id: "pulldown", name: "Lat machine / trazioni", setsTarget: 4, repsTarget: "6-10", rest: 120 },
-            { id: "legcurl", name: "Leg curl", setsTarget: 3, repsTarget: "10-12", rest: 90 },
-            { id: "triceps", name: "Pushdown tricipiti", setsTarget: 2, repsTarget: "10-12", rest: 75 }
+            { id: "pulldown", name: "Lat machine / trazioni", setsTarget: 4, repsTarget: "6-10", rest: 120 }
           ]
         }
       ]
@@ -43,10 +39,7 @@
           name: "Push",
           exercises: [
             { id: "bench", name: "Panca piana", setsTarget: 4, repsTarget: "6-8", rest: 120 },
-            { id: "incline", name: "Panca inclinata manubri", setsTarget: 3, repsTarget: "8-10", rest: 120 },
-            { id: "ohp", name: "Military press", setsTarget: 3, repsTarget: "6-10", rest: 120 },
-            { id: "lateral", name: "Alzate laterali", setsTarget: 3, repsTarget: "12-15", rest: 75 },
-            { id: "triceps", name: "Pushdown tricipiti", setsTarget: 3, repsTarget: "10-12", rest: 75 }
+            { id: "incline", name: "Panca inclinata manubri", setsTarget: 3, repsTarget: "8-10", rest: 120 }
           ]
         },
         {
@@ -54,9 +47,7 @@
           name: "Pull",
           exercises: [
             { id: "pulldown", name: "Lat machine / trazioni", setsTarget: 4, repsTarget: "6-10", rest: 120 },
-            { id: "row", name: "Rematore", setsTarget: 4, repsTarget: "8-10", rest: 120 },
-            { id: "rear", name: "Deltoidi posteriori", setsTarget: 3, repsTarget: "12-15", rest: 75 },
-            { id: "curl", name: "Curl", setsTarget: 3, repsTarget: "10-12", rest: 75 }
+            { id: "row", name: "Rematore", setsTarget: 4, repsTarget: "8-10", rest: 120 }
           ]
         },
         {
@@ -64,9 +55,7 @@
           name: "Legs",
           exercises: [
             { id: "squat", name: "Squat", setsTarget: 4, repsTarget: "5-8", rest: 150 },
-            { id: "rdl", name: "Stacco rumeno", setsTarget: 3, repsTarget: "6-10", rest: 150 },
-            { id: "legpress", name: "Leg press", setsTarget: 3, repsTarget: "10-12", rest: 120 },
-            { id: "calf", name: "Polpacci", setsTarget: 4, repsTarget: "10-15", rest: 75 }
+            { id: "rdl", name: "Stacco rumeno", setsTarget: 3, repsTarget: "6-10", rest: 150 }
           ]
         }
       ]
@@ -81,11 +70,7 @@
           name: "Upper A",
           exercises: [
             { id: "bench", name: "Panca piana", setsTarget: 4, repsTarget: "6-8", rest: 120 },
-            { id: "row", name: "Rematore", setsTarget: 4, repsTarget: "8-10", rest: 120 },
-            { id: "incline", name: "Panca inclinata manubri", setsTarget: 3, repsTarget: "8-10", rest: 120 },
-            { id: "pulldown", name: "Lat machine / trazioni", setsTarget: 3, repsTarget: "6-10", rest: 120 },
-            { id: "triceps", name: "Tricipiti", setsTarget: 2, repsTarget: "10-12", rest: 75 },
-            { id: "curl", name: "Curl", setsTarget: 2, repsTarget: "10-12", rest: 75 }
+            { id: "row", name: "Rematore", setsTarget: 4, repsTarget: "8-10", rest: 120 }
           ]
         },
         {
@@ -93,8 +78,7 @@
           name: "Lower A",
           exercises: [
             { id: "squat", name: "Squat", setsTarget: 4, repsTarget: "5-8", rest: 150 },
-            { id: "legcurl", name: "Leg curl", setsTarget: 3, repsTarget: "10-12", rest: 90 },
-            { id: "calf", name: "Polpacci", setsTarget: 4, repsTarget: "10-15", rest: 75 }
+            { id: "legcurl", name: "Leg curl", setsTarget: 3, repsTarget: "10-12", rest: 90 }
           ]
         },
         {
@@ -102,10 +86,7 @@
           name: "Upper B",
           exercises: [
             { id: "ohp", name: "Military press", setsTarget: 4, repsTarget: "6-10", rest: 120 },
-            { id: "pulldown", name: "Lat machine / trazioni", setsTarget: 4, repsTarget: "6-10", rest: 120 },
-            { id: "dbpress", name: "Panca manubri", setsTarget: 3, repsTarget: "8-10", rest: 120 },
-            { id: "cableRow", name: "Row al cavo", setsTarget: 3, repsTarget: "10-12", rest: 120 },
-            { id: "lateral", name: "Alzate laterali", setsTarget: 3, repsTarget: "12-15", rest: 75 }
+            { id: "pulldown", name: "Lat machine / trazioni", setsTarget: 4, repsTarget: "6-10", rest: 120 }
           ]
         },
         {
@@ -113,9 +94,7 @@
           name: "Lower B",
           exercises: [
             { id: "rdl", name: "Stacco rumeno", setsTarget: 4, repsTarget: "6-10", rest: 150 },
-            { id: "legpress", name: "Leg press", setsTarget: 3, repsTarget: "10-12", rest: 120 },
-            { id: "split", name: "Affondi / Bulgarian", setsTarget: 3, repsTarget: "8-12", rest: 120 },
-            { id: "calf", name: "Polpacci", setsTarget: 4, repsTarget: "10-15", rest: 75 }
+            { id: "legpress", name: "Leg press", setsTarget: 3, repsTarget: "10-12", rest: 120 }
           ]
         }
       ]
@@ -132,10 +111,5 @@
     return TEMPLATES[templateId] || TEMPLATES["tpl-4"];
   }
 
-  KongFit.templates = {
-    TEMPLATES,
-    templateIdForDays,
-    getTemplate
-  };
+  KongFit.templates = { TEMPLATES, templateIdForDays, getTemplate };
 })();
-``
